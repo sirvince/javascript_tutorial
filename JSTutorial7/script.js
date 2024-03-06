e7aa375e25b5327a45c9bad1de1e87b08e3808a7
vincent dallego <Alvingjohndrich@gmail.com>
2024-03-06 09:42:18 +0800

    add function

--- JSTutorial7/script.js
+++ JSTutorial7/script.js
@@ -1,3 +1,13 @@
+let sum = (a, b) => {
+  let result = a + b;
+  return result;
+}
+
+let result1 = sum(5,7);
+console.log(result1); // 12
+
+
+
 let age = 5;
 
 let welcome = (age < 18) ?
@@ -10,17 +20,18 @@ welcome(); // Baby
 
 
 
+
 let greet = x => console.log(x);
-greet(); // Hello
+greet('Hello'); // Hello
 
 
 
 
 
 //Function Expression
-let sum = function (num1,num2){
-  return num1 + num2;
-}
+// let sum = function (num1,num2){
+//   return num1 + num2;
+// }
 //Arrow Function
 let sum1 = (num1,num2) => num1 + num2;
 
